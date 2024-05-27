29f7dd75d072c5a6db5059cd6f139ed3202bdbd6
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-05-27 15:51:52 -0300

    pegando nome do usuário pelo terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::stdin;
+
 fn main() {
+    let mut nome_do_usuario = String::new();
+
+    // recebendo input do usuário
+    stdin().read_line(&mut nome_do_usuario).unwrap();
+
     println!("--- BEM VINDO ---");
+    println!("{nome_do_usuario}")
 }
